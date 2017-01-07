b2897105a7d0e98c60ad69681edd740d6bc76cba
Victor Mours <victor.mours@gmail.com>
2017-01-07 13:55:54 +0100

    Add episode creation

--- app/routes/episodes.js
+++ app/routes/episodes.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return [{ title:  'Tube EM40' },{ title: 'Tube EM39'}];
-  }
 });
--- app/routes/episodes/index.js
+++ app/routes/episodes/index.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.get('store').findAll('episode');
+  },
+
+  actions: {
+    add(){
+      var episode = this.get('store').createRecord('episode', {title: 'Nouvel épisode'});
+      episode.save();
+    }
+  }
 });
